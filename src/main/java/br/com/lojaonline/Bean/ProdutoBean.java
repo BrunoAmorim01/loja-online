b73af38d7d41d68fcc473ea246f5092008882063
Bruno <brunoamorim14@gmail.com>
2016-04-07 17:20:13 -0300

    cadastro produto

--- src/main/java/br/com/lojaonline/Bean/ProdutoBean.java
+++ src/main/java/br/com/lojaonline/Bean/ProdutoBean.java
@@ -8,6 +8,8 @@ import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import org.omnifaces.util.Messages;
+
 import br.com.lojaonline.DAO.CategoriaDAO;
 import br.com.lojaonline.Service.ProdutoService;
 import br.com.lojaonline.model.Categoria;
@@ -16,52 +18,49 @@ import br.com.lojaonline.model.Produto;
 @SuppressWarnings("serial")
 @Named
 @ViewScoped
-public class ProdutoBean implements Serializable{
+public class ProdutoBean implements Serializable {
 
 	@Inject
 	private ProdutoService produtoService;
-	
+
 	@Inject
 	CategoriaDAO categoriaDAO;
-	
+
 	private Produto produto;
-	
+
 	private List<Produto> produtos;
 	private List<Categoria> categorias;
-	
+
 	@PostConstruct
-	public void init(){
-		
-		produtos=produtoService.getProdutoDAO().list("nome");
+	public void init() {
+
+		produtos = produtoService.getProdutoDAO().list("nome");
 	}
-	
-	public void novo(){
-		produto=new Produto();
+
+	public void novo() {
+		produto = new Produto();
 		produto.setQuantidadeEstoque(0);
-		categorias=categoriaDAO.list("nome");
+		categorias = categoriaDAO.list("nome");
 	}
-	
-	public void salvar(){
+
+	public void salvar() {
 		produtoService.salvar(produto);
+		Messages.addGlobalInfo("Produto salvo com sucesso");
 		novo();
 		init();
 	}
-
 
 	public Produto getProduto() {
 		return produto;
 	}
 
-
 	public void setProduto(Produto produto) {
 		this.produto = produto;
 	}
 
-
 	public List<Produto> getProdutos() {
 		return produtos;
 	}
-
 
 	public void setProdutos(List<Produto> produtos) {
 		this.produtos = produtos;
